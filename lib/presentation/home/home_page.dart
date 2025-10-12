1c9de807c82dee21fbf5ecd4de09fea4ff27a4eb
Rohit Kokate <kokate.rohit17@gmail.com>
2025-10-12 15:23:09 +0530

    Update Movie Grid and add two tabs: Now Playing & Trending

--- lib/presentation/home/home_page.dart
+++ lib/presentation/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:binge_box/domain/entities/movie/movie.dart';
 import 'package:binge_box/injectable/service_locator.dart';
 import 'package:binge_box/presentation/home/cubit/home_cubit.dart';
 import 'package:binge_box/presentation/home/cubit/home_state.dart';
@@ -13,12 +14,16 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController;
+
   @override
   void initState() {
     SchedulerBinding.instance.addPostFrameCallback((_) {
       getIt<AppSizes>().init(context);
     });
+    _tabController = TabController(length: 2, vsync: this);
 
     super.initState();
   }
@@ -43,22 +48,26 @@ class _HomePageState extends State<HomePage> {
                 );
               case HomeReady(:final trendingMovies, :final nowPlayingMovies):
                 return Scaffold(
-                  body: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3),
-                    itemBuilder: (context, index) {
-                      final movie = trendingMovies[index];
-                      return Card(
-                        child: Column(
-                          children: [
-                            Text(movie.title),
-                            getIt<AppSizes>().vPadding16,
-                            Image.network(movie.posterPath ?? ''),
-                          ],
-                        ),
-                      );
-                    },
-                    itemCount: trendingMovies.length,
+                  appBar: AppBar(
+                    title: Text('Binge Box'),
+                    bottom: TabBar(
+                      controller: _tabController,
+                      tabs: [
+                        Tab(text: 'Now Playing'),
+                        Tab(text: 'Trending'),
+                      ],
+                    ),
+                  ),
+                  body: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      MovieGrid(
+                        movies: nowPlayingMovies,
+                      ),
+                      MovieGrid(
+                        movies: trendingMovies,
+                      ),
+                    ],
                   ),
                 );
               case HomeError(:final error):
@@ -72,5 +81,78 @@ class _HomePageState extends State<HomePage> {
             }
           },
         ));
+  }
+}
+
+class MovieGrid extends StatelessWidget {
+  const MovieGrid({
+    super.key,
+    required this.movies,
+  });
+
+  final List<Movie> movies;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        mainAxisSpacing: 8,
+        crossAxisSpacing: 8,
+        mainAxisExtent: 232,
+      ),
+      itemBuilder: (context, index) {
+        final movie = movies[index];
+        return Card(
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              AspectRatio(
+                aspectRatio: 9 / 16,
+                child: Image.network(
+                  movie.posterPath ?? '',
+                  fit: BoxFit.cover,
+                  height: 232,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 150,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        Colors.black,
+                      ],
+                      stops: [
+                        0.2,
+                        1.0
+                      ]),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      movie.title ?? 'No Title',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+      itemCount: movies.length,
+    );
   }
 }
