2746a22a2db661246d364c9f0bd7a80d49de6a15
Rohit Kokate <kokate.rohit17@gmail.com>
2025-10-13 16:31:21 +0530

    HomeCubit: Remove unused code.

--- lib/presentation/home/cubit/home_cubit.dart
+++ lib/presentation/home/cubit/home_cubit.dart
@@ -17,36 +17,6 @@ class HomeCubit extends Cubit<HomeState> {
   final GetTrendingMoviesUseCase getTrendingMoviesUseCase;
   final GetNowPlayingMoviesUseCase getNowPlayingMoviesUseCase;
 
-  final int _trendingMoviesPage = 1;
-  final int _nowPlayingMoviesPage = 1;
-
-  List<Movie> trendingMovies = [];
-  List<Movie> nowPlayingMovies = [];
-
-  // Future<void> loadHomeData() async {
-  //   _emitLoading();
-
-  //   final trendingResult = await getTrendingMoviesUseCase(_trendingMoviesPage);
-  //   final nowPlayingResult =
-  //       await getNowPlayingMoviesUseCase(_nowPlayingMoviesPage);
-
-  //   trendingResult.fold(
-  //     (error) => emit(HomeState.error(error)),
-  //     (movieList) {
-  //       trendingMovies = [...trendingMovies, ...movieList.results];
-  //       emitReady();
-  //     },
-  //   );
-
-  //   nowPlayingResult.fold(
-  //     (error) => emit(HomeState.error(error)),
-  //     (movieList) {
-  //       nowPlayingMovies = [...nowPlayingMovies, ...movieList.results];
-  //       emitReady();
-  //     },
-  //   );
-  // }
-
   Future<List<Movie>> fetchTrendingMoviesPage(int pageKey) async {
     final result = await getNowPlayingMoviesUseCase(pageKey);
     return result.fold(
@@ -71,10 +41,6 @@ class HomeCubit extends Cubit<HomeState> {
 
   void goToDetailsPage(Movie movie) {
     emit(HomeState.goToDetailsPage(movie));
-  }
-
-  void _emitLoading() {
-    emit(HomeState.loading());
   }
 
   void emitReady() {
